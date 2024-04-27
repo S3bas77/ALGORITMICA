d1f1fd9665f23ef00e852461f776efad6300243c
Sebastian <sebasnocach@gmail.com>
2024-04-27 17:24:05 -0400

    lista

---
+++ Estructura de datos/List/lista.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define input freopen("in.txt","r",stdin)
+#define output freopen("out.txt","w",stdout)
+
+int main() {
+    output;
+    list<int> lista; 
+    lista.push_back(1);
+    lista.push_back(2);
+    lista.push_back(3);
+    list<int>::iterator iterador; //Se crea un iterador
+    
+    iterador = lista.begin(); 
+    cout << *iterador << endl; 
+    //Dicho iterador irá desde el inicio de la lista definida 
+    //hasta el final de la misma
+    for (iterador = lista.begin(); iterador != lista.end(); iterador++) {
+        cout << *iterador << endl;
+    }
+
+    return 0;
+}
